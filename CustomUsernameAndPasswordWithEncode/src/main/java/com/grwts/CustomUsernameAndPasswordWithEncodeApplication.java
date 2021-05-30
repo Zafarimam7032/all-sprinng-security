e66500911d119e04eb4cdc4e1923cc19bfcaa231
Zafar_imam <Zafar.imam@greatWits.com>
2021-05-30 11:16:06 +0530

    spring security

---
+++ CustomUsernameAndPasswordWithEncode/src/main/java/com/grwts/CustomUsernameAndPasswordWithEncodeApplication.java
@@ -0,0 +1,13 @@
+package com.grwts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomUsernameAndPasswordWithEncodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomUsernameAndPasswordWithEncodeApplication.class, args);
+	}
+
+}
